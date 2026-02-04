ec7a6dec7a8c5d9a4b9b94ef0c422068365db9ed
Untea Jessica <142442146+unteajessica@users.noreply.github.com>
2026-02-04 20:27:44 +0200

    Rename seed_stations.sql file path

---
+++ seed/seed_stations.sql
@@ -0,0 +1,16 @@
+-- =====================================================
+-- Topic: Seed Data - Stations
+-- File: seed/seed_stations.sql
+-- =====================================================
+
+INSERT INTO Station(StationName, City, Capacity)
+VALUES
+('Minerva', 'Cluj-Napoca', 15),
+('Piata Muzeului', 'Cluj-Napoca', 30),
+('Iulius Mall', 'Cluj-Napoca', 25);
+
+INSERT INTO Station (StationName, City, Capacity)
+VALUES
+('Parcul Mare', 'Cluj-Napoca', 50),
+('Platinia', 'Cluj-Napoca', 12),
+('Gara', 'Cluj-Napoca', 8);
