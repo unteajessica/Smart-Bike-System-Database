9180b4009e81a87fd181fd364ee304e62f2c195d
Untea Jessica <142442146+unteajessica@users.noreply.github.com>
2026-02-04 20:48:44 +0200

    Create data generator procedures for testing framework
    Added stored procedures for generating test data.

---
+++ testing_framework/data_generators.sql
@@ -0,0 +1,22 @@
+-- =====================================================
+-- Topic: Performance Testing Framework - Data Generator Procedures
+-- File: 06_testing_framework/02_data_generators.sql
+-- =====================================================
+
+CREATE OR ALTER PROCEDURE usp_GenerateStations @NoOfRows INT
+AS
+BEGIN
+    -- (content from Lab4)
+END;
+
+CREATE OR ALTER PROCEDURE usp_GenerateAlerts @NoOfRows INT
+AS
+BEGIN
+    -- (content from Lab4)
+END;
+
+CREATE OR ALTER PROCEDURE usp_GenerateUserFavoriteStation @NoOfRows INT
+AS
+BEGIN
+    -- (content from Lab4)
+END;
